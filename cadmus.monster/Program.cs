1bb777c1f323d042ae4709826bb25635d170b17a
João Rabelo <joao.filho@cadmus.com.br>
2021-03-12 23:06:11 -0300

    first commit

---
+++ cadmus.monster/Program.cs
@@ -0,0 +1,31 @@
+﻿using cadmus.monster.src;
+using System;
+using System.Reflection;
+
+namespace cadmus.monster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                var versionString = Assembly.GetEntryAssembly()
+                                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                        .InformationalVersion
+                                        .ToString();
+
+                Console.WriteLine($"Monster v{versionString}");
+                Console.WriteLine("-------------");
+                CreateFile.GenerateFile();
+
+                return;
+            } 
+            else
+            {
+                Console.WriteLine("VIA PARAMENTRO COMANDO");
+                CreateFile.GenerateFile();
+            }
+        }
+    }
+}
---
+++ cadmus.monster/src/CreateFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace cadmus.monster.src
+{
+    public static class CreateFile
+    {
+        public static void GenerateFile()
+        {
+            string folderName = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+            string pathString = Path.Combine(folderName, "Repository");
+
+            Directory.CreateDirectory(pathString);
+
+            string fileName = "MyNewFile.cs";
+
+            pathString = Path.Combine(pathString, fileName);
+
+            Console.WriteLine("Path to my file: {0}\n", pathString);
+
+            if (!File.Exists(pathString))
+            {
+                using (FileStream fs = File.Create(pathString))
+                {
+                    for (byte i = 0; i < 100; i++)
+                    {
+                        fs.WriteByte(i);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("File \"{0}\" already exists.", fileName);
+                return;
+            }
+        }
+    }
+}
